4c6e2c8ced3b35b8378dc9b15dbee6afd3d8b7c8
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2023-04-02 20:10:14 -0700

    adding a user controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from " ../models/User.js";
+import User from "../models/User.js";
 
 /* REGISTER USER */
 export const register = async (req, res) => {
@@ -28,7 +28,7 @@ export const register = async (req, res) => {
       friends,
       location,
       occupation,
-      viewedProfile: Math.floor(Math.random() * 1000),
+      viewedProfile: Math.floor(Math.random() * 10000),
       impressions: Math.floor(Math.random() * 10000),
     });
     const savedUser = await newUser.save();
@@ -38,20 +38,21 @@ export const register = async (req, res) => {
   }
 };
 
+/* LOGGING IN */
 export const login = async (req, res) => {
   try {
-      const { email, password } = req.body;
+    const { email, password } = req.body;
     // req.body has properties named email and password and here we are destructuring them to use as variables later
     const user = await User.findOne({ email: email });
-    if (!user) return res.status(400).json({ msg: "user does not exist" });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
-    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
-
-} catch (err) {
+    res.status(200).json({ token, user });
+  } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
